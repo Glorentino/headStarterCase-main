df2215301d260541fac1251b250a2c97b212238f
Glorentino <chrisflorentino96@gmail.com>
2022-08-21 22:40:28 -0400

    settingUpdate

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appID = "a7fb2830a3a8478cb750e1bdb7b2d970";
-const token = "007eJxTYHhUeXNPhsyT7seWU5pDW1M67Cx3TuOrKX44T77qctzse7MUGBLN05KMLIwNEo0TLUzMLZKTzE0NUg2TUpLMk4xSLM0Nyp7+S/pixpBcbKPCxMgAgSA+C0NuYmYeAwMA7jkifg==";
+const token = "007eJxTYOBQe8fz0ef6kqOzyjLLt7F/f9x/eMlPBZ8kx/2eifPSZSIUGBLN05KMLIwNEo0TLUzMLZKTzE0NUg2TUpLMk4xSLM0NWlsYk1mvMSU7rlzEwAiFID4LQ25iZh4DAwCAwiBe";
 
 
 export const config = {mode: "rtc", codec: "vp8", appID: appID, token: token};
